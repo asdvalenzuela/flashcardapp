e3ebb75740ac847360bc30e1678a3c34d36c9e0e
Alaina <asdvalenzuela@gmail.com>
2015-01-20 12:09:43 -0800

    Sqlite database connected and displaying saved flashcards to index.rhtml

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/flashcards.db")
+
+class Flashcard
+  include DataMapper::Resource
+  property :id,           Serial
+  property :word,         String, :required => true
+  property :definition,	  Text, :required => true
+  property :difficulty,	  Integer, :required => true, :min => 1, :max => 10
+end
+DataMapper.finalize
 
 get '/' do
+  @flashcards = Flashcard.all
   erb :index
 end
